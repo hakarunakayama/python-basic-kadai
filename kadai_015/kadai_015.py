e4ded7df2bfca494c215effa1dca210089f37ccb
hakarunakayama <151834966+hakarunakayama@users.noreply.github.com>
2023-12-28 12:53:09 +0900

    引数をメソッドでなくコンストラクタに設定

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -1,12 +1,12 @@
 class Human:
-  def __init__(self):
-    self.name = ""
-    self.age = ""
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-  def printinfo(self, name, age):
-    print(f"私の名前は{name}で、今年で{age}歳です。")
+  def printinfo(self):
+    print(f"私の名前は{self.name}で、今年で{self.age}歳です。")
 
 # コンストラクタで引数にselfしか指定していないので、ここでは引数の値はいらない
-human = Human()
+human = Human("中山意", "40")
 # メソッド定義で引数にnameとageも指定しているので、ここで引数の値が必要
-human.printinfo("中山意", "40")
+human.printinfo()
